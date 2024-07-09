2b1fda2c07c6388111786090fcfc14dc2197757a
SHIVA RAGHAV <139120737+RaghavShiva@users.noreply.github.com>
2024-07-09 18:31:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,14 +11,15 @@ import fileUpload from "express-fileupload";
 
 const app = express();
 config({ path: "./config/config.env" });
-app.use(cors())
-// app.use(
-  // cors({
-  //   origin: [process.env.FRONTEND_URL],
-  //   method: ["GET", "POST", "DELETE", "PUT"],
-  //   credentials: true,
-  // })
-// );
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    method: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization']
+  })
+);
 
 app.use(cookieParser());
 app.use(express.json());
